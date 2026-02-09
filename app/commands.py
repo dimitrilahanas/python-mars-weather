5cbd74ffec3805e10200eadf073fb13efb9475f4
dimitrilahanas <dimitrilahanas@gmail.com>
2026-02-09 15:39:56 +1100

    refactor(Commands): simplified into get_sol_data method.

--- app/commands.py
+++ app/commands.py
@@ -1,4 +1,5 @@
 from mars_data import MarsData
+from models.sol import Sol
 
 class Commands:
     mars_data = MarsData.get_mars_data()
@@ -12,31 +13,13 @@ class Commands:
     def get_current_sol():
         data = Commands.mars_data
         return data["sol_keys"][-1]
-
+    
     @staticmethod
-    def get_sol_temperature(sol: str):
-        data = Commands.mars_data
-        sol_data = data[sol]
-        temperature = sol_data["AT"]
-        return f'{temperature["av"]} degrees C'
-
-    @staticmethod
-    def get_sol_season(sol: str):
-        data = Commands.mars_data
-        sol_data = data[sol]
-        return sol_data["Season"]
-
-    @staticmethod
-    def get_sol_wind_direction(sol: str):
-        data = Commands.mars_data
-        sol_data = data[sol]
-        wind = sol_data["WD"]
-        wind_direction = wind["most_common"]
-        return wind_direction["compass_point"]
-
-    @staticmethod
-    def get_sol_wind_speed(sol: str):
-        data = Commands.mars_data
-        sol_data = data[sol]
-        horizontal_speed = sol_data["HWS"]
-        return f'{horizontal_speed["av"]} m/s'
+    def get_sol_data(sol_num: str):
+        data = Commands.mars_data[sol_num]
+        return Sol (
+            temperature=data["AT"]["av"],
+            season=data["Season"],
+            wind_direction=data["WD"]["most_common"]["compass_point"],
+            wind_speed=["HWS"]["av"]
+        )
